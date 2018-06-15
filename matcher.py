1bf7b01822d333a2ec1cc36bede3e26aaaedf764
Justin Harrison <me@justinharrison.ca>
2018-06-15 08:02:04 -0400

    Handle multiple hanzi

--- matcher.py
+++ matcher.py
@@ -4,6 +4,22 @@ import os, sqlite3
 
 conn = sqlite3.connect(os.path.join(os.path.dirname(os.path.realpath(__file__)), "cedict.db"))
 
+def multiple_hanzi(conn, n, flag):
+    for char in n["Hanzi"]:
+        c = conn.cursor()
+
+        t = (char,)
+        c.execute("SELECT * FROM entries WHERE simplified = ? LIMIT 1", t)
+        entry = c.fetchone()
+        if entry is None:
+            return flag
+        n["Pinyin"] += entry[2] + " "
+
+        c.close()
+    n["Pinyin"] = n["Pinyin"].rstrip()
+
+    return True
+
 # Can't use tab or select another field. You must click outside of a field for this hook to work.
 def on_focus_lost(flag, n, fidx):
     if "Chinese" not in n.model()['name']:
@@ -16,7 +32,8 @@ def on_focus_lost(flag, n, fidx):
     c.execute("SELECT * FROM entries WHERE simplified = ? LIMIT 1", t)
     entry = c.fetchone()
     if entry is None:
-        return flag
+        return multiple_hanzi(conn, n, flag)
+
     n["Pinyin"] = entry[2]
     n["Meaning"] = entry[3].replace("/", "<br>")
     c.close()
